2e4380366511d0ba3b16b78b875dbe8a63cb6532
0619691092 <48439695+0619691092@users.noreply.github.com>
2020-10-21 12:58:29 +0200

    loginFacebook update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,6 @@ import { InicioComponent } from './inicio/inicio.component';
 import { ListaUsuariosComponent } from './lista-usuarios/lista-usuarios.component';
 import { NavbarRegisterComponent } from './components/navbar-register/navbar-register.component';
 import { ButtonLoginComponent } from './components/button-login/button-login.component';
-import { LoginFacebookComponent } from './components/login-facebook/login-facebook.component';
 import { ExploreComponent } from '@core/components/explore/explore.component'
 import { MapComponent } from '@core/components/explore/map/map.component'
 
@@ -27,7 +26,6 @@ import { MapComponent } from '@core/components/explore/map/map.component'
     ListaUsuariosComponent,
     NavbarRegisterComponent,
     ButtonLoginComponent,
-    LoginFacebookComponent,
     ExploreComponent,
     MapComponent
   ],
