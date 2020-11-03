11c92a91445f8356edb56e2df740485334d2f5a3
43685901 <ilm@alu.uma.es>
2020-11-04 00:13:19 +0100

    Update publication.ts

--- src/app/models/publication.ts
+++ src/app/models/publication.ts
@@ -1,14 +1,26 @@
 import { firestore } from 'firebase';
 
 export class Publication {
-    pid:string;
-    uid:string;
-    titulo:string;
-    grafitero:string;
-    fotoUrl:string;
-    location: firestore.GeoPoint;
-    fecha:Date;
-    estado:string;
-    tematicas: string[3];
-    nLikes:Number;
+    private pid:string;
+    private uid:string;
+    private titulo:string;
+    private grafitero:string;
+    private fotoUrl:string;
+    private location: firestore.GeoPoint;
+    private fecha:Date;
+    private estado:string;
+    private tematicas: string[];
+    private nLikes:Number;
+
+    constructor(pid:string,uid: string, titulo: string,grafitero: string,fotourl: string
+        ,location: firestore.GeoPoint,fecha:Date,estado:string,tematicas:string[],nLikes:Number){
+        this.uid = uid;
+        this.estado =estado;
+        this.fecha = fecha;
+        this.fotoUrl = fotourl;
+        this.grafitero=grafitero;
+        this.location = location;
+        this.tematicas = tematicas;
+        this.nLikes = nLikes;
+    }
 }
