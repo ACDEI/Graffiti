57af57c2d5a7c360bec93f63564686eb9b167b20
DeuneB07 <angelbaeza0702@gmail.com>
2020-12-26 20:11:47 +0100

    Admin Puede Editar Publicaciones

--- src/app/services/classes_services/publications.service.ts
+++ src/app/services/classes_services/publications.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument} from '@angular/fire/firestore';
 import { Publication } from '@core/models/publication';
 import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class PublicationsService {
 
   private path = 'publications';
 
-  constructor(private fs: AngularFirestore) { 
+  constructor(private fs: AngularFirestore, private http: HttpClient) { 
     this.publicationCollection = fs.collection(this.path);
   }
 
@@ -40,4 +41,42 @@ export class PublicationsService {
     return this.fs.collection<AngularFirestoreDocument<Publication>>('publications', ref => ref.where('uid', '==', uid)).valueChanges();
   }
 
+  //CLOUD FUNCTIONS
+  private pubsUrl = 'https://us-central1-graffiti-9b570.cloudfunctions.net/APIRest/publications';  // URL to web api
+
+  getPublications(): Observable<any[]>{
+    return this.http.get<any[]>(this.pubsUrl);
+  }
+
+  getPublicationsCount(): Observable<any>{
+    return this.http.get<any>(this.pubsUrl + "/count");
+  }
+
+  getPublicationsByUserEmail(email: string): Observable<any[]>{
+    return this.http.get<any[]>("https://us-central1-graffiti-9b570.cloudfunctions.net/APIRest/users" + this.pubsUrl + "/" + email);
+  }
+
+  getPublicationsFromTo(from: Number, to: Number): Observable<any[]>{
+    return this.http.get<any[]>(this.pubsUrl + "/from/" + from + "/" + to);
+  }
+
+  getPublicationsByGraffiterName(name : String): Observable<any[]>{
+    return this.http.get<any[]>(this.pubsUrl + "/autor/" + name);
+  }
+
+  getPublicationsByTheme(theme : String): Observable<any[]>{
+    return this.http.get<any[]>(this.pubsUrl + "/tematicas/"+ theme);
+  }
+
+  deleteById(pid : string){
+    return this.http.delete(this.pubsUrl + "/" + pid);
+  }
+
+  updateById(pid : string, pub : any ){
+    return this.http.put<any>(this.pubsUrl + "/" + pid, pub);
+  }
+
+  postPublication(pub : any){
+    return this.http.post<any>(this.pubsUrl, pub);
+  }
 }
