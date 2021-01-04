68182a01334ef9bf2e4ec784aaa1b4e644c95263
Carlos <carlostroller99@gmail.com>
2021-01-04 22:51:56 +0100

    Card User Made
    HTML y TS hechos pero no probados

--- src/app/components/cards/card-user/card-user.component.ts
+++ src/app/components/cards/card-user/card-user.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from '@core/models/user.model';
+import { UserService } from '@core/services/classes_services/user.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-card-user',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardUserComponent implements OnInit {
 
-  constructor() { }
+  usuarioSesion: any;
+  @Input() userR: User;
+
+  followedList: any[];
+
+  constructor(private userService: UserService, private ts : ToastrService) { }
 
   ngOnInit(): void {
+    this.usuarioSesion = JSON.parse(window.sessionStorage.getItem("usuario"));
+    this.userService.getFollowedPerUserCF(this.usuarioSesion.uid).subscribe(values => {
+      this.followedList = values;
+    });
+  }
+
+  loSigo(): boolean{
+    var encontrado: boolean = false;
+    if(this.followedList?.length > 0){
+      for(var i = 0; i < this.followedList?.length && !encontrado; i++){
+        if(this.followedList[i]?.uid === this.userR.uid) {
+          encontrado = true;
+        }
+      }
+    }
+    return encontrado;
+  }
+
+  followUser(){
+    var data = {
+        "uid": this.userR.uid,
+        "nick": this.userR.nickName,
+        "image": this.userR.photoURL
+    };
+    this.userService.postFollowedPerUserCF(this.usuarioSesion.uid, data).subscribe(
+      data => { this.ts.info("Ahora sigues a " + this.userR.nickName, "", {timeOut: 1000}); },
+      err => { this.ts.error("Ups...", "Ha Habido un problema al seguir." 
+        + " Pruebe de nuevo más tarde", {timeOut: 1000}); }
+    );
+  }
+
+  unfollowUser(){
+    this.userService.deleteFollowedPerUserCF(this.usuarioSesion.uid, this.userR.uid).subscribe(
+      data => { this.ts.success("Ha dejado de seguir a " + this.userR.uid, "", {timeOut: 1000}); },
+      err => { this.ts.error("Ups...", "Ha Habido un problema al dejar de seguir." 
+        + " Pruebe de nuevo más tarde", {timeOut: 1000}); }
+    );
   }
 
 }
