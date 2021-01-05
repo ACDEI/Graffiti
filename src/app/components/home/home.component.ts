42cee98f15aaead4bb916308e08a488645b60b05
DeuneB07 <angelbaeza0702@gmail.com>
2021-01-05 11:24:38 +0100

    Paginacion Anterior Puesta de nuevo

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -34,15 +34,8 @@ export class HomeComponent implements OnInit {
   //token firebase
   token:string; 
 
-  // Para paginacion:
-  firstInResponse : any = [];  //First Document Retrieved
-  lastInResponse : any = [];  //Last Document Retrieved
-  prev_start_at : any = [];  //Documents of Previous Pages
-  pageSize = 8;  //Items Per Page
-  totalItems : any;
-
   config: any = {
-    itemsPerPage: this.pageSize,
+    itemsPerPage: 12,
     currentPage: 1,
     totalItems: 0
   }
@@ -61,7 +54,7 @@ export class HomeComponent implements OnInit {
 
     this.isFilter = false;
     //this.resetear();
-    this.getPublicationsPaginateFirst();
+    this.getPublications();
     this.obtenerTematicas();
 
   }
@@ -122,93 +115,14 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  getPublicationsPaginateFirst(){
-    this.fs.collection('publications', ref => ref.limit(this.pageSize).orderBy('date', 'desc'))
-    .snapshotChanges().subscribe( response => {
-      if(!response.length) return;  //No Data
-      this.firstInResponse = response[0].payload.doc;
-      this.lastInResponse = response[response.length - 1].payload.doc;
-
-      this.publicationsList = [];
-      this.publicationsList = response.map(item => {
-        return {
-          id : item.payload.doc.id,
-          data : item.payload.doc.data()
-        }
-      });
-
-      this.pubService.getPublicationCount().then( value => {
-        this.config.totalItems = value;
-        console.log(this.totalItems)
-      })
-
-      //Initialize Values
-      this.prev_start_at = [];
-      //this.pag_clicked_count = 0;
-      this.config.totalItems = this.totalItems;
-
-      //Push First Item to Use for Previous Action
-      this.prev_start_at.push(this.firstInResponse);
-    });
-  }
-
-  prevPage() {
-    this.fs.collection('publications', 
-        ref => ref.limit(this.pageSize).orderBy('date', 'desc')
-                  .startAt(this.get_prev_startAt())
-                  .endBefore(this.firstInResponse))
-      .snapshotChanges().subscribe( response => {
-        if(!response.length) return;  //No Data
-        this.firstInResponse = response[0].payload.doc;
-        this.lastInResponse = response[response.length - 1].payload.doc;
-
-        this.publicationsList = [];
-        this.publicationsList = response.map(item => {
-          return {
-            id : item.payload.doc.id,
-            data : item.payload.doc.data()
-          }
-        });
-
-        //Push First Item to Use for Previous Action
-        this.prev_start_at.forEach(element => {
-          if (this.firstInResponse.data().id == element.data().id) element = null;
-        });
-      });
-  }
-
-  nextPage() {
-    this.fs.collection('publications', ref => ref.limit(this.pageSize)
-        .orderBy('date', 'desc').startAfter(this.lastInResponse))
-      .snapshotChanges().subscribe( response => {
-        if(!response.length) return;  //No Data
-        this.firstInResponse = response[0].payload.doc;
-        this.lastInResponse = response[response.length - 1].payload.doc;
-
-        this.publicationsList = [];
-        this.publicationsList = response.map(item => {
-          return {
-            id : item.payload.doc.id,
-            data : item.payload.doc.data()
-          }
-        });
-
-        //Push First Item to Use for Previous Action
-        this.prev_start_at.push(this.firstInResponse);
-      });
-  }
-
-  //Return the Doc rem where previous page will startAt
-  get_prev_startAt() {
-    if (this.prev_start_at.length > (this.config.currentPage))
-      this.prev_start_at.splice(this.prev_start_at.length - 2, this.prev_start_at.length - 1);
-    return this.prev_start_at[this.config.currentPage];
+  getPublications(){
+    this.pubService.getAllPublicationsCF().subscribe(data => {
+      this.publicationsList = data;
+      this.config.totalItems = this.publicationsList.length;
+    })
   }
 
   pageChanged(event){
-    var pageC = this.config.currentPage;
-    if(event > pageC) this.nextPage();
-    else this.prevPage();
     this.config.currentPage = event;
   }
 
--- src/app/components/users-search/users-search.component.ts
+++ src/app/components/users-search/users-search.component.ts
@@ -20,15 +20,8 @@ export class UsersSearchComponent implements OnInit {
   email: string;
   nickName: string;
 
-  // Para paginacion:
-  firstInResponse : any = [];  //First Document Retrieved
-  lastInResponse : any = [];  //Last Document Retrieved
-  prev_start_at : any = [];  //Documents of Previous Pages
-  pageSize = 8;  //Items Per Page
-  totalItems : any;
-
   config: any = {
-    itemsPerPage: this.pageSize,
+    itemsPerPage: 12,
     currentPage: 1,
     totalItems: 0
   };
@@ -36,7 +29,11 @@ export class UsersSearchComponent implements OnInit {
   constructor(private userService: UserService, private fs : AngularFirestore, private http: HttpClient) { }
 
   ngOnInit(): void {
-    this.getUsersPaginateFirst();
+    this.userService.getUserByAdminCF(0).subscribe(data =>{
+      this.usersList = data;
+      this.config.totalItems = this.usersList.length;
+      this.usersList = this.usersList.filter(user => user.uid != JSON.parse(window.sessionStorage.getItem("usuario")).uid);
+    })
     this.isFilter = false;
   }
 
@@ -62,93 +59,7 @@ export class UsersSearchComponent implements OnInit {
     
   }
 
-  getUsersPaginateFirst(){
-    this.fs.collection('users', ref => ref.limit(this.pageSize).orderBy('fullName', 'asc'))
-    .snapshotChanges().subscribe( response => {
-      if(!response.length) return;  //No Data
-      this.firstInResponse = response[0].payload.doc;
-      this.lastInResponse = response[response.length - 1].payload.doc;
-
-      this.usersList = [];
-      this.usersList = response.map(item => {
-        return {
-          id : item.payload.doc.id,
-          data : item.payload.doc.data()
-        }
-      });
-
-      this.userService.getUsersCount().then( value => {
-        this.config.totalItems = value;
-        console.log(this.totalItems)
-      })
-
-      //Initialize Values
-      this.prev_start_at = [];
-      //this.pag_clicked_count = 0;
-      this.config.totalItems = this.totalItems;
-
-      //Push First Item to Use for Previous Action
-      this.prev_start_at.push(this.firstInResponse);
-    });
-  }
-
-  prevPage() {
-    this.fs.collection('users', 
-        ref => ref.limit(this.pageSize).orderBy('fullName', 'asc')
-                  .startAt(this.get_prev_startAt())
-                  .endBefore(this.firstInResponse))
-      .snapshotChanges().subscribe( response => {
-        if(!response.length) return;  //No Data
-        this.firstInResponse = response[0].payload.doc;
-        this.lastInResponse = response[response.length - 1].payload.doc;
-
-        this.usersList = [];
-        this.usersList = response.map(item => {
-          return {
-            id : item.payload.doc.id,
-            data : item.payload.doc.data()
-          }
-        });
-
-        //Push First Item to Use for Previous Action
-        this.prev_start_at.forEach(element => {
-          if (this.firstInResponse.data().id == element.data().id) element = null;
-        });
-      });
-  }
-
-  nextPage() {
-    this.fs.collection('users', ref => ref.limit(this.pageSize)
-        .orderBy('fullName', 'asc').startAfter(this.lastInResponse))
-      .snapshotChanges().subscribe( response => {
-        if(!response.length) return;  //No Data
-        this.firstInResponse = response[0].payload.doc;
-        this.lastInResponse = response[response.length - 1].payload.doc;
-
-        this.usersList = [];
-        this.usersList = response.map(item => {
-          return {
-            id : item.payload.doc.id,
-            data : item.payload.doc.data()
-          }
-        });
-
-        //Push First Item to Use for Previous Action
-        this.prev_start_at.push(this.firstInResponse);
-      });
-  }
-
-  //Return the Doc rem where previous page will startAt
-  get_prev_startAt() {
-    if (this.prev_start_at.length > (this.config.currentPage))
-      this.prev_start_at.splice(this.prev_start_at.length - 2, this.prev_start_at.length - 1);
-    return this.prev_start_at[this.config.currentPage];
-  }
-
   pageChanged(event){
-    var pageC = this.config.currentPage;
-    if(event > pageC) this.nextPage();
-    else this.prevPage();
     this.config.currentPage = event;
   }
 
