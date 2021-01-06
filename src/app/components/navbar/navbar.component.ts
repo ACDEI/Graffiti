05037ba45e4551ac30e400bfdc4b5dcbd6ac728c
DeuneB07 <angelbaeza0702@gmail.com>
2021-01-06 20:18:00 +0100

    Upload FlickrService

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '@core/services/auth.service';
 import { PublicationsService } from '@core/services/classes_services/publications.service';
 import { ThemeService } from '@core/services/classes_services/theme.service';
 import { UserService } from '@core/services/classes_services/user.service';
+import { FlickrService } from '@core/services/flickr.service';
 import * as firebase from 'firebase';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
 import { map } from 'rxjs/operators';
@@ -19,7 +20,7 @@ export class NavbarComponent implements OnInit {
   //Usuario Sesion
   usuarioSesion: any;
 
-  //Form
+  //Form Initialize
   stateList : string[] = ['Perfect', 'Legible', 'Illegible', 'Deteriorated'];
   themesList: any[];
   themesSettings : IDropdownSettings = {
@@ -31,14 +32,17 @@ export class NavbarComponent implements OnInit {
     defaultOpen: false
   };
 
+  //Form Variables
   statusSelector: string;
   themesSelector: string[];
   title: string;
   graffiter: string;
   urlFoto: string; 
+  latSelected : any;
+  lngSelected : any;
 
   //Flickr
-  conectadoFlickr = false ; 
+  conectadoFlickr = false; 
   selectedFile: File = null;
   showButton = false; 
   oauth_token:string;
@@ -48,7 +52,8 @@ export class NavbarComponent implements OnInit {
 
   constructor(private auth: AuthService, private http: HttpClient, 
       private route: Router , private ps: PublicationsService, 
-      private userService: UserService, private themeService: ThemeService) {
+      private userService: UserService, private themeService: ThemeService, 
+      private fs : FlickrService) {
    }
 
   ngOnInit(): void {
@@ -91,8 +96,9 @@ export class NavbarComponent implements OnInit {
   }
 
   async conectarFlickr(){
-
-
+    this.fs.conectarFlickr(window.location.href);
+    console.log(this.fs.tokenFirebase)
+    /* QUITAR CUANDO SEPAMOS QUE FUNCIONA
     let self = this; 
     await firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
       // Send token to your backend via HTTPS
@@ -114,28 +120,43 @@ export class NavbarComponent implements OnInit {
   
     result.subscribe(data =>{
       console.log(data.url);
-      /// si el usuario acepta flickr devolverá un callback a la home del usuario en malagart
+      ///Si el usuario acepta flickr devolverá un callback a la home del usuario en malagart
       //this.route.navigateByUrl(data.url);
       window.location.href = data.url; 
     })
-
+    */
   }
 
 
   async subirImagen(){
 
-     const formData : FormData = new FormData();  
-     formData.append('file', this.selectedFile, this.selectedFile.name);
-     formData.append('oauth_token',this.oauth_token);
-     formData.append('oauth_verifier', this.oauth_verifier);
-     formData.append('title',this.title);
- 
+    const formData : FormData = new FormData();  
+    formData.append('file', this.selectedFile, this.selectedFile.name);
+    formData.append('oauth_token', this.oauth_token);
+    formData.append('oauth_verifier', this.oauth_verifier);
+    formData.append('title', this.title);
+    
+    let photo = { 
+      "state": this.statusSelector, 
+      "themes": this.themesSelector,
+      "title": this.title, 
+      "graffiter": this.graffiter , 
+      "photoURL": this.urlFoto , 
+      "uid": this.usuarioSesion.uid, 
+      "pid": this.generatePID(),
+      "lat": this.latSelected,
+      "lng": this.lngSelected
+    }
 
+    this.fs.subirFlickr(formData, photo);
+    /*  QUITAR CUANDO SEPAMOS QUE FUNCIONA
+
+     let photo : any;
      let url = "http://localhost:5001/graffiti-9b570/us-central1/MalagArtApiWeb/flickr/upload";
      let result = await this.http.post<any>(url,formData);
      let self = this; 
      
-     result.subscribe(data =>{
+     await result.subscribe(data =>{
         //self.idFoto = data.id ;
         console.log(data);
         self.urlFoto = "https://live.staticflickr.com/"+ data.photo.server + "/"+ data.photo.id +"_"+ data.photo.secret +".jpg" ;
@@ -143,7 +164,7 @@ export class NavbarComponent implements OnInit {
 
         let d = new Date();
          // TODO lat y long
-        let photo : any = { 
+        photo = { 
           "state": this.statusSelector, 
           "themes": this.themesSelector,
           "title": this.title, 
@@ -151,16 +172,15 @@ export class NavbarComponent implements OnInit {
           "photoURL": this.urlFoto , 
           "uid": this.usuarioSesion.uid, 
           "pid": this.generatePID(),
-          //"lat": ,
-          //"lon": 
-          
+          "lat": this.latSelected,
+          "lng": this.lngSelected
         }
-         
-        this.ps.postPublicationCF(photo);
+        
+        this.ps.postPublicationCF(photo).subscribe();
       })
-
+      
      this.showButton = false; 
-   
+   */
    }
 
    onFileSelected(event){
---
+++ src/app/services/flickr.service.ts
@@ -0,0 +1,74 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import * as firebase from 'firebase';
+import { AuthService } from './auth.service';
+import { PublicationsService } from './classes_services/publications.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FlickrService {
+
+  //OAuth
+  tokenFirebase : string;
+
+  constructor(private http: HttpClient, private auth: AuthService, 
+      private ps : PublicationsService) {
+    this.oauthUser();
+  }
+
+  private flickUrl = "http://localhost:5001/graffiti-9b570/us-central1/MalagArtApiWeb/flickr/";
+
+  conectarFlickr(url) {
+
+    if(this.tokenFirebase == null) this.oauthUser();
+    const httpOpt = {
+      headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenFirebase }),
+      params: new HttpParams().set('url', url)
+    }
+    let result = this.http.get<any>(this.flickUrl + 'conectar', httpOpt);
+    result.subscribe(data =>{
+      //console.log('dataURL: ' + data.url);
+      ///Si el usuario acepta flickr devolverá un callback a la home del usuario en malagart
+      window.location.href = data.url; 
+    })
+  }
+
+  async subirFlickr(formData, photo) {
+
+    if(this.tokenFirebase == null) this.oauthUser();
+    const httpOpt = {
+      headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenFirebase })
+    }
+
+    let result = await this.http.post<any>(this.flickUrl + 'upload', formData, httpOpt);
+
+    await result.subscribe(data =>{
+      //self.idFoto = data.id ;
+      //console.log(data);
+      let urlFoto = "https://live.staticflickr.com/"+ data.photo.server + "/"+ data.photo.id +"_"+ data.photo.secret +".jpg" ;
+      //console.log(urlFoto);
+
+      photo = { ...photo, 'photoURL' : urlFoto};
+      
+      this.ps.postPublicationCF(photo).subscribe();
+    })
+
+
+  }
+
+  //Autenticar Usuario
+  async oauthUser(){
+    let tokenFb : string;
+    await firebase.auth().currentUser.getIdToken(true).then(function(idToken) {
+      // Send token to your backend via HTTPS
+      tokenFb = idToken;
+      //console.log(idToken);
+    }).catch(function(error) {
+      // Handle error
+    });
+    this.tokenFirebase = tokenFb;
+    return tokenFb;
+  }
+
+}
