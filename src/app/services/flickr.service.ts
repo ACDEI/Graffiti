1e1abbc4b49b3f8a94ff64435857b50263cef5ad
edugemen <edugemen@uma.es>
2021-01-07 13:02:53 +0100

    Update flickr.service.ts

--- src/app/services/flickr.service.ts
+++ src/app/services/flickr.service.ts
@@ -22,22 +22,23 @@ export class FlickrService {
   private flickUrl = "http://localhost:5001/graffiti-9b570/us-central1/MalagArtApiWeb/flickr/";
 
   conectarFlickr(url) {
+    let self = this;
 
     if(this.tokenFirebase == null) this.oauthUser();
     const httpOpt = {
       headers: new HttpHeaders({'Authorization': 'Bearer ' + this.tokenFirebase }),
       params: new HttpParams().set('url', url)
     }
-    let result = this.http.get<any>(this.flickUrl + 'conectar', httpOpt);
-    result.subscribe(data =>{
-      //console.log('dataURL: ' + data.url);
+    this.http.get<any>(this.flickUrl + 'conectar', httpOpt).toPromise().then( data => {
+      console.log('dataURL: ' + data.url, 'tsecret', + data.token_secret);
+      self.flickr_oauth_token_secret = data.token_secret;
       ///Si el usuario acepta flickr devolverá un callback a la home del usuario en malagart
       window.location.href = data.url; 
-    })
+    });
   }
 
   async subirFlickr(formData, photo) {
-    console.log()
+    console.log("tsecret", this.flickr_oauth_token_secret);
 
     if(this.tokenFirebase == null) this.oauthUser();
     const httpOpt = {
@@ -47,7 +48,7 @@ export class FlickrService {
     //console.log('ABC: ' + photo);
     let result = await this.http.post<any>(this.flickUrl + 'upload', formData, httpOpt);
 
-    await result.subscribe(data =>{
+    await result.toPromise().then(data =>{
       //self.idFoto = data.id ;
       //console.log(data);
       let urlFoto = "https://live.staticflickr.com/"+ data.photo.server + "/"+ data.photo.id +"_"+ data.photo.secret +".jpg" ;
