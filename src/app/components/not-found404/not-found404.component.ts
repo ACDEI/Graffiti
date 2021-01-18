0859b10bb0ee92be212c234ffe0903d6c9f9eefe
Carlos <carlostroller99@gmail.com>
2021-01-18 22:34:05 +0100

    404 diseño listo, pero no funciona por el ActivatedRoute

--- src/app/components/not-found404/not-found404.component.ts
+++ src/app/components/not-found404/not-found404.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRouteSnapshot, Router } from '@angular/router';
+import { AuthService } from '@core/services/auth.service';
 
 @Component({
   selector: 'app-not-found404',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotFound404Component implements OnInit {
 
-  constructor() { }
+  logged: string;
+  usuarioSesion: any = null;
+
+  constructor(private authService: AuthService, private route: ActivatedRouteSnapshot, private router: Router) { }
 
   ngOnInit(): void {
+  if (this.authService.isLoggedIn()) {
+      const userRole = this.authService.getRole();  //ADMIN o USER
+      if (this.route.data.role && this.route.data.role.indexOf(userRole) === -1) {
+        if(userRole === 'ADMIN') this.logged = "a";
+        else if(userRole === 'USER') {
+          this.usuarioSesion = JSON.parse(window.sessionStorage.getItem("usuario"));
+          this.logged = "b";
+        }
+        else { this.logged = "c"; }
+      }
+      this.logged = "c";
+    }
+    this.logged = "c";
   }
+
 
 }
